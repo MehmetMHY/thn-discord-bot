198972f29d71fa6a935709f9aaa673925f453e23
Dylan Eck <dylaneck@mymail.mines.edu>
2022-02-11 18:49:48 -0700

    fixed recent message collection

--- main.py
+++ main.py
@@ -21,18 +21,22 @@ async def on_ready():
     print(f'{client.user} has connected to Discord')
     update.start()    
 
+async def get_recent_msg(channel, mlimit):
+    recent_urls = []
+    async for message in channel.history(limit=mlimit):
+        recent_urls.append(message.content)
+    return recent_urls
+
 @tasks.loop(hours=1)
 async def update():
-    # channel = client.get_channel(941375372556120120)
-
     channel = discord.utils.find(lambda c: c.name == "all-news", client.get_all_channels())
     
-    recent_urls = []
-    async for message in channel.history(limit=100):
-        recent_urls.append(message.content)
+    recent_urls = await get_recent_msg(channel, 100)
 
     data = thnapi.get_thn_data()
+
     new_data = [x for x in data if x["url"] not in recent_urls]
+    new_data.reverse()
 
     if(new_data):
         for item in new_data:
