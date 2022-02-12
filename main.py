45b3ea01f84b83f85957c1e562cc851f81332ced
Dylan Eck <dylaneck@mymail.mines.edu>
2022-02-11 21:25:17 -0700

    added helper funtions for filtering

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import discord
 from discord.ext import tasks
 from dotenv import load_dotenv
@@ -19,7 +20,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord')
-    update.start()    
+    update.start()
 
 async def get_recent_msg(channel, mlimit):
     recent_urls = []
@@ -30,7 +31,7 @@ async def get_recent_msg(channel, mlimit):
 @tasks.loop(hours=1)
 async def update():
     channel = discord.utils.find(lambda c: c.name == "all-news", client.get_all_channels())
-    
+
     recent_urls = await get_recent_msg(channel, 100)
 
     data = thnapi.get_thn_data()
@@ -39,9 +40,43 @@ async def update():
     new_data.reverse()
 
     if(new_data):
-        for item in new_data:
-            await channel.send(item["url"])
-    
+        f = open("./filter_words/stop_words.txt") # words that we don't care about
+        stop_words = f.readlines()
+        f.close()
+        stop_words = [x.lower().strip() for x in stop_words]
+
+        for article in new_data:
+            words = article["title"].split() + article["details"].split()
+            words = [x.lower() for x in words]
+
+            keywords = []
+            for word in words:
+                if word not in stop_words:
+                    keywords.append(word)
+            print(keywords)
+
+def filter_by_keywords(articles, keywords):
+    for article in articles:
+        words = article["title"].split() + article["details"].split()
+
+def load_filter_words():
+    files = get_file_names_regex("./filter_words", "*.txt")
+    words = []
+    for file in files:
+        f = open(file)
+        words += f.readlines()
+        f.close()
+    return words
+
+def get_file_names_regex(directory, pattern):
+    file_names = []
+    for (root, dirs, files) in os.walk(directory):
+        for file in files:
+            pattern = re.compile(pattern)
+            if pattern.match(file):
+                file_names.append(file)
+    return file_names
+
 client.run(TOKEN)
 
 
