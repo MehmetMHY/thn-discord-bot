f4df82783f4d672718de78c564e2d59254773333
Mehmet <mehmet.mhy@gmail.com>
2022-02-27 17:40:02 -0700

    Updated thn_api.py to filter the title better and included article's publish date in return

--- thn_api.py
+++ thn_api.py
@@ -30,12 +30,17 @@ def get_thn_data(optimal=False):
 
         # filter out article's title:
         title = line[5]
-        title = title[title.find('<img alt=\"')+len('<img alt=\"'):title.find('\" loading=')]
+        title = title[title.find('<img alt=')+len('<img alt='):title.find(' loading=')]
+        title = title[1:-1] # remove quotation
+
+        # filter out article's published date:
+        date = line[9]
+        date = date[date.find('</i>')+len('</i>'):date.find('<span><i')]
 
         # filter out article's detail (some of it's detail):
         detail = line[11]
         detail = detail[detail.find('<div class="home-desc"> ')+len('<div class="home-desc"> '):detail.find('</div>')]
 
-        cdata.append({"url": url, "title": title, "details": detail})
+        cdata.append({"url": url, "title": title, "date": date, "details": detail})
     
     return cdata
